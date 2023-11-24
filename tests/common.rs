976b0282d15a99a82b2c373d3f53757a4f0ebf50
Jakub Kubík <jakub.kubik.it@protonmail.com>
2023-11-25 00:18:24 +0100

    test: use rstest instead of tokio_test

--- tests/common.rs
+++ tests/common.rs
@@ -1,41 +1,20 @@
 use bytes::{BufMut, Bytes, BytesMut};
 use etherparse::PacketBuilder;
-use ipnet::IpNet;
-use once_cell::sync::Lazy;
-use quincy::interface::{Interface, InterfaceRead, InterfaceWrite};
+use quincy::config::{ClientConfig, FromPath, ServerConfig};
+use quincy::interface::{InterfaceRead, InterfaceWrite};
+use rstest::fixture;
 use std::io::Error;
 use std::net::Ipv4Addr;
+use std::path::Path;
 use std::pin::Pin;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::task::{Context, Poll};
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
-use tokio::sync::mpsc;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tokio::sync::Mutex;
 
-type TestSender = Arc<Mutex<UnboundedSender<Bytes>>>;
-type TestReceiver = Arc<Mutex<UnboundedReceiver<Bytes>>>;
-pub static TEST_QUEUE_CLIENT_SEND: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
-    let (tx, rx) = mpsc::unbounded_channel();
-    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
-});
-
-pub static TEST_QUEUE_CLIENT_RECV: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
-    let (tx, rx) = mpsc::unbounded_channel();
-    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
-});
-
-pub static TEST_QUEUE_SERVER_SEND: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
-    let (tx, rx) = mpsc::unbounded_channel();
-    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
-});
-
-pub static TEST_QUEUE_SERVER_RECV: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
-    let (tx, rx) = mpsc::unbounded_channel();
-    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
-});
-
-pub struct Client;
-pub struct Server;
+pub type TestSender = Arc<Mutex<UnboundedSender<Bytes>>>;
+pub type TestReceiver = Arc<Mutex<UnboundedReceiver<Bytes>>>;
 
 pub struct TestInterface<T> {
     _p: std::marker::PhantomData<T>,
@@ -43,8 +22,15 @@ pub struct TestInterface<T> {
     rx: TestReceiver,
 }
 
-pub type TestInterfaceClient = TestInterface<Client>;
-pub type TestInterfaceServer = TestInterface<Server>;
+impl<T> TestInterface<T> {
+    pub fn new(tx: TestSender, rx: TestReceiver) -> Self {
+        Self {
+            _p: std::marker::PhantomData,
+            tx,
+            rx,
+        }
+    }
+}
 
 impl<T: Unpin> AsyncRead for TestInterface<T> {
     fn poll_read(
@@ -52,7 +38,7 @@ impl<T: Unpin> AsyncRead for TestInterface<T> {
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        let data = self.get_mut().rx.lock().unwrap().poll_recv(cx);
+        let data = self.get_mut().rx.try_lock().unwrap().poll_recv(cx);
         match data {
             Poll::Ready(Some(data)) => {
                 buf.put_slice(&data);
@@ -71,7 +57,7 @@ impl<T: Unpin> AsyncWrite for TestInterface<T> {
         buf: &[u8],
     ) -> Poll<Result<usize, Error>> {
         let data = Bytes::copy_from_slice(buf);
-        self.get_mut().tx.lock().unwrap().send(data).unwrap();
+        self.get_mut().tx.try_lock().unwrap().send(data).unwrap();
         Poll::Ready(Ok(buf.len()))
     }
 
@@ -86,25 +72,6 @@ impl<T: Unpin> AsyncWrite for TestInterface<T> {
 
 impl<T: 'static + Send + Sync + Unpin> InterfaceRead for TestInterface<T> {}
 impl<T: 'static + Send + Sync + Unpin> InterfaceWrite for TestInterface<T> {}
-impl Interface for TestInterface<Client> {
-    fn create(_interface_address: IpNet, _mtu: i32) -> anyhow::Result<Self> {
-        Ok(Self {
-            _p: std::marker::PhantomData,
-            tx: TEST_QUEUE_CLIENT_SEND.0.clone(),
-            rx: TEST_QUEUE_CLIENT_RECV.1.clone(),
-        })
-    }
-}
-
-impl Interface for TestInterface<Server> {
-    fn create(_interface_address: IpNet, _mtu: i32) -> anyhow::Result<Self> {
-        Ok(Self {
-            _p: std::marker::PhantomData,
-            tx: TEST_QUEUE_SERVER_SEND.0.clone(),
-            rx: TEST_QUEUE_SERVER_RECV.1.clone(),
-        })
-    }
-}
 
 pub fn dummy_packet(src: Ipv4Addr, dest: Ipv4Addr) -> Bytes {
     let mut writer = BytesMut::new().writer();
@@ -114,4 +81,14 @@ pub fn dummy_packet(src: Ipv4Addr, dest: Ipv4Addr) -> Bytes {
         .unwrap();
 
     writer.into_inner().into()
+}
+
+#[fixture]
+pub fn client_config() -> ClientConfig {
+    ClientConfig::from_path(Path::new("tests/static/client.toml"), "QUINCY").unwrap()
+}
+
+#[fixture]
+pub fn server_config() -> ServerConfig {
+    ServerConfig::from_path(Path::new("tests/static/server.toml"), "QUINCY").unwrap()
 }
--- tests/test_end_to_end.rs
+++ tests/test_end_to_end.rs
@@ -1,84 +1,104 @@
 use crate::common::{
-    dummy_packet, TestInterfaceClient, TestInterfaceServer, TEST_QUEUE_CLIENT_RECV,
-    TEST_QUEUE_CLIENT_SEND, TEST_QUEUE_SERVER_RECV, TEST_QUEUE_SERVER_SEND,
+    client_config, dummy_packet, server_config, TestInterface, TestReceiver, TestSender,
 };
+use anyhow::Result;
+use ipnet::IpNet;
+use once_cell::sync::Lazy;
 use quincy::client::QuincyClient;
-use quincy::config::{ClientConfig, FromPath, ServerConfig};
+use quincy::config::{ClientConfig, ServerConfig};
+use quincy::interface::Interface;
 use quincy::server::QuincyServer;
+use rstest::rstest;
 use std::net::Ipv4Addr;
-use std::path::Path;
-use tokio::task::spawn_blocking;
+use std::sync::Arc;
+use tokio::sync::{mpsc, Mutex};
 
 mod common;
 
-#[test]
-fn test_end_to_end_communication() {
+pub static TEST_QUEUE_CLIENT_SEND: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
+    let (tx, rx) = mpsc::unbounded_channel();
+    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
+});
+
+pub static TEST_QUEUE_CLIENT_RECV: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
+    let (tx, rx) = mpsc::unbounded_channel();
+    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
+});
+
+pub static TEST_QUEUE_SERVER_SEND: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
+    let (tx, rx) = mpsc::unbounded_channel();
+    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
+});
+
+pub static TEST_QUEUE_SERVER_RECV: Lazy<(TestSender, TestReceiver)> = Lazy::new(|| {
+    let (tx, rx) = mpsc::unbounded_channel();
+    (Arc::new(Mutex::new(tx)), Arc::new(Mutex::new(rx)))
+});
+
+struct Client;
+struct Server;
+impl Interface for TestInterface<Client> {
+    fn create(_interface_address: IpNet, _mtu: i32) -> Result<Self> {
+        Ok(Self::new(
+            TEST_QUEUE_CLIENT_SEND.0.clone(),
+            TEST_QUEUE_CLIENT_RECV.1.clone(),
+        ))
+    }
+}
+
+impl Interface for TestInterface<Server> {
+    fn create(_interface_address: IpNet, _mtu: i32) -> Result<Self> {
+        Ok(Self::new(
+            TEST_QUEUE_SERVER_SEND.0.clone(),
+            TEST_QUEUE_SERVER_RECV.1.clone(),
+        ))
+    }
+}
+
+#[rstest]
+#[tokio::test]
+async fn test_end_to_end_communication(client_config: ClientConfig, server_config: ServerConfig) {
     #[cfg(target_os = "macos")]
     use quincy::interface::prepend_packet_info_header;
 
-    let client_config =
-        ClientConfig::from_path(Path::new("tests/static/client.toml"), "QUINCY").unwrap();
     let client = QuincyClient::new(client_config);
-
-    let server_config =
-        ServerConfig::from_path(Path::new("tests/static/server.toml"), "QUINCY").unwrap();
     let server = QuincyServer::new(server_config).unwrap();
 
     let ip_server = Ipv4Addr::new(10, 0, 0, 1);
     let ip_client = Ipv4Addr::new(10, 0, 0, 2);
 
-    tokio_test::block_on(async move {
-        tokio::spawn(async move { server.run::<TestInterfaceServer>().await.unwrap() });
-        tokio::spawn(async move { client.run::<TestInterfaceClient>().await.unwrap() });
+    tokio::spawn(async move { server.run::<TestInterface<Server>>().await.unwrap() });
+    tokio::spawn(async move { client.run::<TestInterface<Client>>().await.unwrap() });
 
-        // Test client -> server
-        let test_packet = dummy_packet(ip_client, ip_server);
-        #[cfg(target_os = "macos")]
-        let test_packet = prepend_packet_info_header(test_packet).unwrap();
+    // Test client -> server
+    let test_packet = dummy_packet(ip_client, ip_server);
+    #[cfg(target_os = "macos")]
+    let test_packet = prepend_packet_info_header(test_packet).unwrap();
 
-        TEST_QUEUE_CLIENT_RECV
-            .0
-            .lock()
-            .unwrap()
-            .send(test_packet.clone())
-            .unwrap();
-
-        let recv_packet = spawn_blocking(|| {
-            TEST_QUEUE_SERVER_SEND
-                .1
-                .lock()
-                .unwrap()
-                .blocking_recv()
-                .unwrap()
-        })
+    TEST_QUEUE_CLIENT_RECV
+        .0
+        .lock()
         .await
+        .send(test_packet.clone())
         .unwrap();
 
-        assert_eq!(test_packet, recv_packet);
+    let recv_packet = TEST_QUEUE_SERVER_SEND.1.lock().await.recv().await.unwrap();
 
-        // Test server -> client
-        let test_packet = dummy_packet(ip_server, ip_client);
-        #[cfg(target_os = "macos")]
-        let test_packet = prepend_packet_info_header(test_packet).unwrap();
+    assert_eq!(test_packet, recv_packet);
 
-        TEST_QUEUE_SERVER_RECV
-            .0
-            .lock()
-            .unwrap()
-            .send(test_packet.clone())
-            .unwrap();
+    // Test server -> client
+    let test_packet = dummy_packet(ip_server, ip_client);
+    #[cfg(target_os = "macos")]
+    let test_packet = prepend_packet_info_header(test_packet).unwrap();
 
-        let recv_packet = spawn_blocking(|| {
-            TEST_QUEUE_CLIENT_SEND
-                .1
-                .lock()
-                .unwrap()
-                .blocking_recv()
-                .unwrap()
-        })
+    TEST_QUEUE_SERVER_RECV
+        .0
+        .lock()
         .await
+        .send(test_packet.clone())
         .unwrap();
 
-        assert_eq!(test_packet, recv_packet);
-    });
+    let recv_packet = TEST_QUEUE_CLIENT_SEND.1.lock().await.recv().await.unwrap();
+
+    assert_eq!(test_packet, recv_packet);
 }
