509d914e25bed133edc79dc62ddb943f6da0789e
Jakub Kubík <jakub.kubik.it@protonmail.com>
2024-02-14 18:52:09 +0100

    feat: switch to jemalloc on supported platforms

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 pub mod auth;
 pub mod client;
 pub mod config;
