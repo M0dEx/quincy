b0f8d4ae6527262d1e15376ea624a4ab8e98e6f0
Jakub Kubík <jakub.kubik.it@protonmail.com>
2024-02-25 23:33:15 +0100

    [#46] fix(client, server): correctly log errors during config/app init (#47)

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,26 +1,34 @@
+use std::process::exit;
+
 use anyhow::Result;
 use clap::Parser;
 use quincy::client::QuincyClient;
 use quincy::config::{ClientConfig, FromPath};
 use quincy::utils::cli::Args;
-use quincy::utils::tracing::enable_tracing;
+use quincy::utils::tracing::log_subscriber;
 use tracing::error;
 use tun2::AsyncDevice;
 
 #[tokio::main]
 async fn main() {
-    let args: Args = Args::parse();
+    // Enable default tracing to log errors before the configuration is loaded.
+    let _logger = tracing::subscriber::set_default(log_subscriber("info"));
 
-    match run_client(args).await {
+    match run_client().await {
         Ok(_) => {}
-        Err(e) => error!("A critical error occurred: {e}"),
+        Err(e) => {
+            error!("A critical error occurred: {e}");
+            exit(1);
+        }
     }
 }
 
 /// Runs the Quincy client.
-async fn run_client(args: Args) -> Result<()> {
+async fn run_client() -> Result<()> {
+    let args = Args::try_parse()?;
     let config = ClientConfig::from_path(&args.config_path, &args.env_prefix)?;
-    enable_tracing(&config.log.level);
+    // Enable tracing with the log level from the configuration.
+    tracing::subscriber::set_global_default(log_subscriber(&config.log.level))?;
 
     let client = QuincyClient::new(config);
     client.run::<AsyncDevice>().await
