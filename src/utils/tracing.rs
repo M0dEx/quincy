af915c3080f5626457400094b852baeb6a9d97ac
Jakub Kubík <jakub.kubik.it@protonmail.com>
2024-04-01 23:14:23 +0200

    fix(utils/tracing): use correct formatting on Windows

--- src/utils/tracing.rs
+++ src/utils/tracing.rs
@@ -1,5 +1,5 @@
 use tracing::Subscriber;
-use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::fmt::SubscriberBuilder;
 use tracing_subscriber::EnvFilter;
 
 /// Returns a new `tracing` subscriber with the specified log level.
@@ -7,9 +7,17 @@ use tracing_subscriber::EnvFilter;
 /// ### Arguments
 /// - `log_level` - the log level to use
 pub fn log_subscriber(log_level: &str) -> impl Subscriber {
-    let registry = tracing_subscriber::Registry::default();
-    let fmt_layer = tracing_subscriber::fmt::Layer::new();
+    // Enable ANSI color support on Windows.
+    #[cfg(windows)]
+    let with_ansi = nu_ansi_term::enable_ansi_support().is_ok();
+
+    #[cfg(not(windows))]
+    let with_ansi = false;
+
     let filter_layer = EnvFilter::try_new(log_level).unwrap();
 
-    registry.with(filter_layer).with(fmt_layer)
+    SubscriberBuilder::default()
+        .with_env_filter(filter_layer)
+        .with_ansi(with_ansi)
+        .finish()
 }
